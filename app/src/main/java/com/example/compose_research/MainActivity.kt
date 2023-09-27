b93b9ba7c2bf1ffa45adfd73be945528e670dfd2
Dumankrg2019 <52251067+Dumankrg2019@users.noreply.github.com>
2023-09-28 02:34:11 +0600

    feat: first build

---
+++ app/src/main/java/com/example/compose_research/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.compose_research
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.compose_research.ui.InstagramProfileCard
+import com.example.compose_research.ui.theme.Compose_researchTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose_researchTheme {
+                // A surface container using the 'background' color from the theme
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background),
+                ) {
+                    InstagramProfileCard()
+                    //TestImage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Compose_researchTheme {
+       InstagramProfileCard()
+    }
+}
+
+@Preview
+@Composable
+fun showText() {
+    Text(
+        text = "Hello",
+        fontSize = 24.sp,
+        fontWeight = FontWeight.ExtraBold,
+        fontFamily = FontFamily.Serif,
+        color = Color.Green
+    )
+}
+
+@Preview
+@Composable
+fun TestImage() {
+    Image(
+        modifier = Modifier
+            .size(48.dp),
+        painter = painterResource(id = R.drawable.insta),
+        contentDescription = "Instagram",
+        contentScale = ContentScale.Fit)
+}

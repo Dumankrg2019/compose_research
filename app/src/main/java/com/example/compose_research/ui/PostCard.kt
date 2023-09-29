5c151dd023948ec2f1bc13c879e17464cbbbac39
Dumankrg2019 <52251067+Dumankrg2019@users.noreply.github.com>
2023-09-30 01:41:54 +0600

    feat: add post markup

--- app/src/main/java/com/example/compose_research/ui/PostCard.kt
+++ app/src/main/java/com/example/compose_research/ui/PostCard.kt
@@ -1,6 +1,7 @@
 package com.example.compose_research.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -19,8 +20,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,41 +35,115 @@ fun PostCard() {
     Card(
 
     ) {
-        Row(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .padding(8.dp)
         ) {
+            PostHeader()
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = stringResource(R.string.template_text))
+            Spacer(modifier = Modifier.width(8.dp))
             Image(
-                modifier = Modifier
-                    .size(50.dp)
-                    .clip(CircleShape),
-                painter = painterResource(id = R.drawable.post_comunity_thumbnail),
-                contentDescription = "group image"
+                modifier = Modifier.fillMaxWidth(),
+                painter = painterResource(id = R.drawable.post_content_image),
+                contentDescription = "post",
+                contentScale = ContentScale.FillWidth
             )
             Spacer(modifier = Modifier.width(8.dp))
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-            ) {
-                Text(
-                    text = "dev/null",
-                    fontSize = 12.sp,
-                    color = MaterialTheme.colorScheme.onSecondary
-                )
-                Spacer(modifier = Modifier.width(4.dp))
-                Text(
-                    text = "14:00",
-                    fontSize = 12.sp,
-                    color = MaterialTheme.colorScheme.onSecondary
-                )
-            }
-            Icon(
-                imageVector = Icons.Rounded.MoreVert,
-                contentDescription = "settings",
-                tint = MaterialTheme.colorScheme.onSecondary
+            Statistics()
+        }
+
+    }
+}
+
+@Preview
+@Composable
+private fun PostHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape),
+            painter = painterResource(id = R.drawable.post_comunity_thumbnail),
+            contentDescription = "group image"
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Text(
+                text = "dev/null",
+                fontSize = 12.sp,
+                color = MaterialTheme.colorScheme.onSecondary
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = "14:00",
+                fontSize = 12.sp,
+                color = MaterialTheme.colorScheme.onSecondary
             )
         }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = "settings",
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun Statistics() {
+    Row {
+
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            IconWithText(
+                iconResId = R.drawable.ic_views_count,
+                text = "967"
+            )
+        }
+        Row(
+            modifier = Modifier.weight(1f),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconWithText(
+                iconResId = R.drawable.ic_share,
+                text = "7"
+            )
+            IconWithText(
+                iconResId = R.drawable.ic_comment,
+                text = "8"
+            )
+            IconWithText(
+                iconResId = R.drawable.ic_like,
+                text = "10"
+            )
+        }
+    }
+}
+
+@Composable
+private fun IconWithText(
+    iconResId: Int,
+    text: String
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(
+                id = iconResId),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(text = text, color = MaterialTheme.colorScheme.onSecondary)
     }
 }

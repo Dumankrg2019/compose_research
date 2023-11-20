5e3ba3615ea9dfd6b9556078b12a47b8fb700217
Dumankrg2019 <52251067+Dumankrg2019@users.noreply.github.com>
2023-11-20 23:22:46 +0600

    feat: add activityForResult in compose

--- app/src/main/java/com/example/compose_research/MainActivity.kt
+++ app/src/main/java/com/example/compose_research/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.compose_research.ui.ActivityResultTest
 import com.example.compose_research.ui.VkNewsMS
 import com.example.compose_research.ui.theme.Compose_researchTheme
 
@@ -46,6 +47,7 @@ class MainActivity : ComponentActivity() {
                     //CustomOutlineButton()
                     //CustomDialog()
                     VkNewsMS()
+                    //ActivityResultTest()
                     //ListComments(viewModel)
                     //MainScreen()
                    // InstagramProfileCard(viewModel)
